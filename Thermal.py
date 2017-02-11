7a5712fb48b58658f63224fece51ecb7b9f7200f
akoski <akoski@kth.se>
2017-02-11 01:01:49 +0100

    Method to calculate diameter on a thermal

--- Thermal.py
+++ Thermal.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import math
 
 class Thermal(object):
     """docstring for Thermal."""
@@ -21,13 +22,55 @@ class Thermal(object):
             pTemp = (float(dataPoint[2])/float(dataPoint[3]))
             if 0 < pTemp:
                 self.powersum+=(float(dataPoint[2])/float(dataPoint[3]))
-        self.dataList.append(dataPoint)
+
+        self.dataList.append([float(dataPoint[0]), float(dataPoint[1]), int(dataPoint[2]), int(dataPoint[3])])
 
 
     def getGroundCenter(self):
         #Calculate GC
         pass
-        
+
+    def getDiameter(self):
+        if(len(self.dataList) < 3):
+            return 0
+        try:
+            kStart = (self.dataList[1][0]-self.dataList[0][0])/(self.dataList[1][1]-self.dataList[0][1])
+        except ZeroDivisionError:
+            kStart = (self.dataList[1][0]-self.dataList[0][0])/(self.dataList[1][1]-self.dataList[0][1]+0.00001)
+        try:
+            kNext = (self.dataList[2][0]-self.dataList[1][0])/(self.dataList[2][1]-self.dataList[1][1])
+        except ZeroDivisionError:
+            kNext = (self.dataList[2][0]-self.dataList[1][0])/(self.dataList[2][1]-self.dataList[1][1]+0.00001)
+
+        i = 3
+        done = False
+        if kStart < kNext:
+            while i < len(self.dataList):
+                try:
+                    kNext = (self.dataList[i][0]-self.dataList[i-1][0])/(self.dataList[i][1]-self.dataList[i-1][1])
+                    if kNext < kStart:
+                        done = True
+                        break
+
+                except ZeroDivisionError:
+                    pass
+                i+=1
+        else:
+            while i < len(self.dataList):
+                try:
+                    kNext = (self.dataList[i][0]-self.dataList[i-1][0])/(self.dataList[i][1]-self.dataList[i-1][1])
+                    if kStart < kNext:
+                        done = True
+                        break
+
+                except ZeroDivisionError:
+                    pass
+                i+=1
+        if done:
+            return 111699 * math.sqrt((self.dataList[i][0] - self.dataList[0][0])**2 + (self.dataList[i][1] - self.dataList[0][1])**2)
+        else:
+            return 0
+
     def getPower(self):
         try:
             p = self.powersum/(len(self.dataList)-1)
--- paintThermalMap.py
+++ paintThermalMap.py
@@ -7,6 +7,13 @@ import sys
 
 from Thermal import Thermal
 
+weakThermals = []
+mediumThermals = []
+hardThermals = []
+veryHardThermals = []
+extremeThermals = []
+onceInALifeTimeThermals = []
+
 def getCoords():
     ifile = open('res.txt', "rb")
     reader = csv.reader(ifile)
@@ -28,7 +35,7 @@ def getCoords():
             if abs(latitude[-1] - float(row[0])) < 0.1:
                 if abs(longitude[-1] - float(row[1])) < 0.1:
                     if int(row[3]) < 60 and 0 < int(row[3]):
-                        thermals[-1].addDataPoint(row)
+                        thermals[-1].addDataPoint([float(row[0]), float(row[1]), int(row[2]), int(row[3])])
                         sameThermal = True
         '''
                     else:
@@ -41,7 +48,23 @@ def getCoords():
         else:
             print("Altitude!")
         '''
-        if not sameThermal: #Om vi inte är i samma blåsa som tidigare, skapa en ny blåsa!
+        #Sortera in termikblåsorna i kategorierr efter styrka
+        if not sameThermal: #Om vi inte är i samma blåsa som tidigare, skapa en ny blåsa! men innan det, spara den gammla blåsan i rätt lista
+            if len(thermals) == 0:
+                pass
+            elif thermals[-1].getPower() < 1:
+                weakThermals.append(thermals[-1])
+            elif thermals[-1].getPower() < 2:
+                mediumThermals.append(thermals[-1])
+            elif thermals[-1].getPower() < 3:
+                hardThermals.append(thermals[-1])
+            elif thermals[-1].getPower() < 4:
+                veryHardThermals.append(thermals[-1])
+            elif thermals[-1].getPower() < 5:
+                extremeThermals.append(thermals[-1])
+            else:
+                onceInALifeTimeThermals.append(thermals[-1])
+
             thermals.append(Thermal())
             thermals[-1].addDataPoint(row)
 
@@ -50,7 +73,7 @@ def getCoords():
         latitude.append(float(row[0]))
 
     for i in thermals:
-        print(str(i.getPower())+", "+ str(i.getClimb())+", "+ str(i.getNumberOfDataPoints()))
+        print(str(i.getPower())+", "+ str(i.getClimb())+", "+ str(i.getDiameter())+", "+ str(i.getNumberOfDataPoints()))
     print(len(thermals))
     print(rowCount)
 
@@ -81,12 +104,20 @@ def main():
     x,y = map(lon, lat)
     print("Klar med omvandlingen!")
 
-    '''
+    color1 = '#FFDCAC' #Beige typ
+    color2 = '#FFE454' #Yellowwww!
+    color3 = '#FF7131' # Orange
+    color4 = '#E42F27' #Röd
+    color5 = '#BF0118' # Vinröd
+
+
     plt.gcf().set_size_inches(width, height)
     map.arcgisimage(service='Canvas/World_Light_Gray_Base', xpixels=xpixels)
-    map.scatter(x, y, c='r', alpha=0.005, marker='s', s=50, linewidths=0)
+    map.scatter(x, y, color=color4, alpha=0.005, marker='o', s=100, linewidths = 0)
+
+
 
     plt.show()
-    '''
+
 
 main()
