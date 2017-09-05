79d19556eed8101824603c81ff90377a17cac3e1
akoski <akoski@kth.se>
2017-09-05 10:09:22 +0200

    sdgs

--- extr.py
+++ extr.py
@@ -32,13 +32,19 @@ sys.setdefaultencoding("utf-8")
 s = requests.session()
 s.keep_alive = False
 
-def whatisthis(s):
-    if isinstance(s, str):
-        print "ordinary string"
-    elif isinstance(s, unicode):
-        print "unicode string"
-    else:
-        print "not a string"
+colisionTable = {}
+
+def checkColission(key, value):
+    try:
+        if value != colisionTable[key]:
+            print "\n\n\n\n\n\n\n-----\n\n\n\n\n\n"
+            raise ValueError("Colission: " + str(value) + " - " + str(colisionTable[key]))
+        else:
+            print "Fanns redan: " + value + " - " + str(colisionTable[key])
+        print "No Collision"
+    except KeyError:
+        print "Lägger till pilot " + value
+        colisionTable[key] = value
 
 def findThermalOnGround(x1, y1, z1, x2, y2, z2):
     ##Approximates thermal origin on ground using standard linear algebra
@@ -52,26 +58,45 @@ def approxThermal(x1, y1, x2, y2):
     return ((x2-x1)/2) + x1, ((y2-y1)/2) + y1
 
 def uploadThermalsToElastic(body):
-    if body == "":
-        return 0
     url = "http://37.139.3.211:9200/map/thermals/_bulk"
-    response = s.request("POST", url, data = body)
-    jsonRes = json.loads(response.text)
-    if jsonRes['errors']:
-        print jsonRes
-        print "Error in upload! :O"
+    tries = 5
+
+    while tries != 0:
+        if body == "":
+            return 0
+        response = s.request("POST", url, data = body)
+        jsonRes = json.loads(response.text)
+        if jsonRes['errors']:
+            print jsonRes
+            tries -= 1
+        else:
+            break
+    if tries:
+        return 0
+    else:
         return 1
-    return 0
 
 def extract_data_from_file(filename):
     with open(filename, 'r') as content_file:
         content = content_file.read()
 
+    content = content.replace("<br>", "-") #Fixar buggen med attpilotlistan blir för stor pga tvåmannalag
+    content = content.replace("sterdalarn...", "sterdalarnas FK") #Fixar buggen med attpilotlistan blir för stor pga tvåmannalag
+    content = content.replace("Hultsfed Se...", "Hultsfred Segelflygklubb") #Fixar buggen med attpilotlistan blir för stor pga tvåmannalag
 
     tree = html.fromstring(content)
     igc_download_list = tree.xpath("//td/a[@title='Ladda ner IGC-fil']/@href")
     club_list = tree.xpath("//td/a[@title='Visa klubbens samtliga resultat']/text()")
     pilot_list = tree.xpath("//td/a[contains(@title, 'ttningens samtliga resultat')]/text()")
+
+    print "Listornas längder:"
+    print len(igc_download_list)
+    print len(club_list)
+    print len(pilot_list)
+    print " ---------- "
+    if (len(igc_download_list) != len(club_list)) or (len(club_list) != len(pilot_list)):
+        print len(igc_download_list), len(club_list), len(pilot_list)
+        raise ValueError
     return igc_download_list, pilot_list, club_list
 
 def extract_data_from_url(url):
@@ -116,6 +141,16 @@ def upload_flights_from_igc_links(igc_download_list, pilot_list, club_list, star
             igcURL = "http://www.rst-online.se/" + i ##Link address to igc file
 
         flightID = i[-4:]
+
+        ## ------------------------------- FILTER ------------------------------ ##
+        """
+        if club_list[downloads] != "Ö Sörmlands Fk":
+            downloads+=1
+
+            continue
+        """
+        ## ------------------------------- ------ ------------------------------ ##
+
         tries = 5
         while tries != 0:
             try:
@@ -134,8 +169,16 @@ def upload_flights_from_igc_links(igc_download_list, pilot_list, club_list, star
                 errfile.write(flightID + ", " + str(flight.notes)+"\n")
             continue
 
+
         print hash2.hasha(pilot_list[downloads].encode('utf8')).encode('utf8')
         print pilot_list[downloads].encode('utf8')
+        print club_list[downloads].encode('utf8')
+
+        checkColission(hash2.hasha(pilot_list[downloads].encode('utf8')).encode('utf8'), pilot_list[downloads].encode('utf8'))
+
+        print " --- "
+        print "  "
+
         ## Start-longitude, Start-latitude, Start-höjd, Slut-longitude, Slut-latitude, Slut-höjd, avg-hast, höjdvinst
         for t in flight.thermals:
             thermalCounter+=1
@@ -150,7 +193,7 @@ def upload_flights_from_igc_links(igc_download_list, pilot_list, club_list, star
 
             if 0 < t.vertical_velocity():
                 bulkReq = bulkReq + "{\"index\":{\"_id\":\"" + createIndex( flightID, thermalCounter) +"\"}}\n"
-                bulkReq = bulkReq + "{ \"type\" : \"Feature\" , \"properties\" : {\"velocity\":"+ str(t.vertical_velocity()) + ", \"pilot\": \"" + hash2.hasha(pilot_list[downloads].encode('utf8')).encode('utf8') + "\", \"club\": \"" + club_list[downloads].encode('utf8') + "\"}, \"geometry\":{\"type\":\"Point\", \"coordinates\": [ " + str(x) + ", " + str(y) + " ]}}\n"
+                bulkReq = bulkReq + "{ \"type\" : \"Feature\" , \"properties\" : {\"velocity\":"+ str(t.vertical_velocity()) + ", \"pilot\":\"" + hash2.hasha(pilot_list[downloads].encode('utf8')).encode('utf8') + "\", \"club\": \"" + club_list[downloads].encode('utf8') + "\"}, \"geometry\":{\"type\":\"Point\", \"coordinates\": [ " + str(x) + ", " + str(y) + " ]}}\n"
                 #Atom slutar med syntax highlight på långa strängar...
 
                 #print "{\"index\":{}}"
@@ -172,7 +215,8 @@ def main():
     downloads = 0
     f = open("where.txt", "r")
     startAt = int(f.readline().strip())
-    print startAt
+    print "Börjar hämta flygning nr "+ str(startAt)
+
     f.close()
     error_flights = 0
     failed_uploads = 0
@@ -185,10 +229,15 @@ def main():
         downloads += d
         error_flights += e
         failed_uploads += f
+        print " ---------- "
 
     print str(downloads) + " - Total IGC files fetched from RST"
     print str(error_flights) + " - Errors in extracting thermals from flights"
     print str(failed_uploads) + " - Errors in uploading packages to Elasticsearch"
+
+    f = open("where.txt", "w")
+    f.write("0")
+    f.close()
 
     print "Klaar MFS!!!"
 
