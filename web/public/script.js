38b7a1cc2ed903aa5c4f97a056e4ec4e0e47fa15
akoski <akoski@kth.se>
2018-02-06 08:28:40 +0100

    Fix: Does not reload thermals on zoom-in, closes #16

--- web/public/script.js
+++ web/public/script.js
@@ -3,7 +3,7 @@ var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/aeakoski/cj4bd0ycx4cgx2sptdzigrgtl',
     center: [17.211814, 59.101584], //Vängsö
-    zoom: 11,
+    zoom: 10,
     maxZoom:12,
     minZoom:8
 });
@@ -79,11 +79,16 @@ map.on('moveend', function(){
   let epsilon = 0.000001;
   //Check if map has actually moved
   if(Math.abs(prev_upperLat - lonlat._ne.lat)<epsilon && Math.abs(prev_lowerLat - lonlat._sw.lat)<epsilon && Math.abs(prev_lowerLon - lonlat._sw.lng)<epsilon && Math.abs(prev_upperLon - lonlat._ne.lng)<epsilon){ return; }
+    if ((prev_upperLat >= lonlat._ne.lat) && (prev_lowerLat <= lonlat._sw.lat) && (prev_lowerLon <= lonlat._sw.lng) && (prev_upperLon >= lonlat._ne.lng)) {
+      //We have zoomed in, no new fetch is neccesary
+    }else {
+        sendRequest()
+    }
     prev_upperLat = lonlat._ne.lat;
     prev_lowerLat = lonlat._sw.lat;
     prev_lowerLon = lonlat._sw.lng;
     prev_upperLon = lonlat._ne.lng;
-    sendRequest()
+
 });
 map.on('load', sendRequest)
 map.dragRotate.disable(); //Disables rotation
@@ -95,14 +100,22 @@ var addPointsToMap = function(jsonThermals){
         map.removeSource("thermals");
       }
 
-      document.getElementById('nrt').innerHTML = jsonThermals.features.length;
+      let numberOfThermals = jsonThermals.features.length;
+
+      if (numberOfThermals == 10000) {
+        document.getElementById('nrt').innerHTML = "10000 (max)";
+      }else{
+        document.getElementById('nrt').innerHTML = numberOfThermals;
+      }
+
+      // Display the number of thermals on the screen
 
       map.addSource("thermals", {
           type: "geojson",
           data: jsonThermals,
           cluster: true,
-          clusterMaxZoom: 9, // Max zoom to cluster points on
-          clusterRadius: 15 // Use small cluster radius for the heatmap look
+          clusterMaxZoom: 13, // Max zoom to cluster points on
+          clusterRadius: 5 // Use small cluster radius for the heatmap look
       });
 
 
@@ -112,26 +125,27 @@ var addPointsToMap = function(jsonThermals){
           "source": "thermals",
 
           "paint": {
-              "circle-color": 'rgba(229,36,62, 0.4)',
+              "circle-color": 'rgba(229,36,62, 0.1)',
               "circle-radius": {
                 property: "velocity",
                 type: "exponential",
                 stops: velStop
               },
-              "circle-blur": 1 // blur the circles to get a heatmap look
+              "circle-blur": 1.5 // blur the circles to get a heatmap look
           },
-          "filter": ["<","velocity", 1]
+          "filter": ["<","velocity", 1.2]
           //"filter": ["all",["==", "cluster", true],]
       }, 'waterway-label');
 
 
       map.addLayer({
+        // Blåsor med medelhastighet större än 1
           "id": "cluster-2",
           "type": "circle",
           "source": "thermals",
 
           "paint": {
-              "circle-color": 'rgba(229,36,62, 0.5)',
+              "circle-color": 'rgba(229,36,62, 0.21)',
               "circle-radius": {
                 property: "velocity",
                 type: "exponential",
@@ -139,7 +153,7 @@ var addPointsToMap = function(jsonThermals){
               },
               "circle-blur": 1 // blur the circles to get a heatmap look
           },
-          "filter": [">","velocity", 1]
+          "filter": [">","velocity", 1.2]
       }, 'waterway-label');
 
       map.addLayer({
@@ -148,18 +162,18 @@ var addPointsToMap = function(jsonThermals){
         source: "thermals",
         filter: ["has", "point_count"],
         paint: {
-            "circle-blur": 1.2,
+            "circle-blur": 7,
             "circle-color": {
                 property: "point_count",
                 type: "exponential",
                 stops: [
-                    [0, "rgba(229,36,62, 0.0)"],
-                    [5, "rgba(229,36,62, 0.22)"],
-                    [10, "rgba(198,27,49,0.24)"],
-                    [20, "rgba(198,27,49,0.26)"],
-                    [30, "rgba(198,27,49,0.28)"],
-                    [40, "rgba(198,27,49,0.30)"],
-                    [50, "rgba(198,27,49,0.30)"]
+                  [0, "rgba(229,36,62, 0.0)"],
+                  [5, "rgba(229,36,62, 0.3)"],
+                  [10, "rgba(229,36,62,0.4)"],
+                  [20, "rgba(229,36,62,0.5)"],
+                  [30, "rgba(229,27,49,0.6)"],
+                  [40, "rgba(229,27,49,0.6)"],
+                  [50, "rgba(229,27,49,0.6)"]
                 ]
             },
             "circle-radius": {
@@ -167,12 +181,12 @@ var addPointsToMap = function(jsonThermals){
                 type: "exponential",
                 stops: [
                   [0, 0],
-                  [5, 40],
-                  [10, 45],
-                  [20, 50],
-                  [30, 55],
-                  [40, 60],
-                  [50, 65]
+                 [5, 70],
+                 [10, 80],
+                 [20, 90],
+                 [30, 100],
+                 [40, 100],
+                 [50, 100]
                 ]
             }
         }
